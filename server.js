d7a2737479d5394429b80fa77d1499443fc20fa3
HydraFire <hydrafire.plus@gmail.com>
2018-05-23 18:39:14 +0300

    new start

---
+++ server.js
@@ -0,0 +1,39 @@
+
+require('dotenv').load();
+// /////////////////////////////////////////////////////////////////////////////
+const WebSocketServer = require('ws').Server;
+const https = require('https');
+const http = require('http');
+const express = require('express');
+const fs = require('fs');
+// Модули программы
+const { webSocketOnConnect } = require('./aska_script/webSocketOnConnect');
+const { SmartTrain } = require('./aska_script/NN/differenceNN');
+const { mainTimeCircle } = require('./aska_script/mainTimeCircle');
+//
+const exp = express();
+exp.use(express.static(`${__dirname}/public`));
+//
+const key = fs.readFileSync('./private.key');
+const cert = fs.readFileSync('./primary.crt');
+const ca = fs.readFileSync('./intermediate.crt');
+const options = {
+  key,
+  cert,
+  ca
+};
+//
+//
+// http Server
+// http.createServer(exp).listen(80);
+// https server
+const server = https.createServer(options, exp).listen(process.env.PORT);
+// WebSocketServer
+const wss = new WebSocketServer({ server });
+webSocketOnConnect(wss);
+// Тренеруэм нейроную сеть если обновились команды
+SmartTrain();
+// Главный цикл обслуживает все задания и напоминания
+setInterval(() => {
+  mainTimeCircle();
+}, 60000 * 5);
