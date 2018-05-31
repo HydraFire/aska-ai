0559dc63dbbfe0e05ef287404e9d9764e710de55
HydraFire <hydrafire.plus@gmail.com>
2018-05-31 17:06:48 +0300

    fix

--- mobile/index.js
+++ mobile/index.js
@@ -1,12 +1,28 @@
 // let's go!
 import './css/logotype.css';
-import WebSocketClient from './components/webSocketClient';
+import './css/inputCommandLine.css';
+import socket from './components/webSocketClient';
+import DisplayWordsClass from './components/interface/displayWordsClass';
 import { speechRec, startStopRec } from './components/speechRecognition';
 
 
-WebSocketClient.start();
+socket.start();
 speechRec();
 
 const askaButton = document.querySelector('#projectName');
 console.log(askaButton);
 askaButton.addEventListener('click', startStopRec);
+
+
+const display = new DisplayWordsClass();
+const commandLine = document.querySelector('.inputCommandLine');
+commandLine.onkeydown = function onkeydown(e) {
+  if (e.keyCode === 13) {
+    if (this.value !== '') {
+      display.displayWords(this.value);
+      display.displayWordsFinal('🎤');
+      socket.send(this.value);
+      this.value = '';
+    }
+  }
+};
