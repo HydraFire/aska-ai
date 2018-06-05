230bc999a0c4d87b42d305b023635a858c508670
HydraFire <hydrafire.plus@gmail.com>
2018-06-05 17:41:49 +0300

    fix

--- mobile/index.js
+++ mobile/index.js
@@ -1,6 +1,8 @@
 // let's go!
+import ScrollSnap from 'scroll-snap';
 import './css/logotype.css';
 import './css/inputCommandLine.css';
+
 import socket from './components/webSocketClient';
 import DisplayWordsClass from './components/interface/displayWordsClass';
 import { speechRec, startStopRec } from './components/speechRecognition';
@@ -26,5 +28,34 @@ commandLine.onkeydown = function onkeydown(e) {
     }
   }
 };
+/*
+const askaButton = document.querySelector('#main_container');
 
+askaButton.addEventListener('touchend', () => {
+  console.log('ok');
+  window.scrollTo(360,0)
+});
+askaButton.addEventListener('touchcancel', () => {
+  console.log('ok');
+  window.scrollTo(360,0)
+});
+*/
+
+
+const snapConfig = {
+  scrollSnapDestination: '100% 0%', // *REQUIRED* scroll-snap-destination css property, as defined here: https://developer.mozilla.org/en-US/docs/Web/CSS/scroll-snap-destination
+  scrollTimeout: 100, // *OPTIONAL* (default = 100) time in ms after which scrolling is considered finished
+  scrollTime: 300 // *OPTIONAL* (default = 300) time in ms for the smooth snap
+}
+
+function callback () {
+  console.log('called when snap animation ends')
+}
+
+const element = document.getElementById('container');
+console.log(element);
+const snapObject = new ScrollSnap(element, snapConfig)
+console.log(snapObject);
+
+snapObject.bind(callback)
 Kaleidoscope();
