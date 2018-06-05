9a4cb59bfd56480dbff8ee183eafc06cfa4c5f72
HydraFire <hydrafire.plus@gmail.com>
2018-06-05 17:56:33 +0300

    fix

--- mobile/index.js
+++ mobile/index.js
@@ -10,11 +10,10 @@ import Kaleidoscope from './Kaleidoscope';
 
 socket.start();
 speechRec();
-/*
-const askaButton = document.querySelector('#projectName');
-console.log(askaButton);
+
+const askaButton = document.querySelector('#draw');
 askaButton.addEventListener('click', startStopRec);
-*/
+
 
 const display = new DisplayWordsClass();
 const commandLine = document.querySelector('.inputCommandLine');
@@ -28,34 +27,19 @@ commandLine.onkeydown = function onkeydown(e) {
     }
   }
 };
-/*
-const askaButton = document.querySelector('#main_container');
-
-askaButton.addEventListener('touchend', () => {
-  console.log('ok');
-  window.scrollTo(360,0)
-});
-askaButton.addEventListener('touchcancel', () => {
-  console.log('ok');
-  window.scrollTo(360,0)
-});
-*/
-
 
 const snapConfig = {
-  scrollSnapDestination: '100% 0%', // *REQUIRED* scroll-snap-destination css property, as defined here: https://developer.mozilla.org/en-US/docs/Web/CSS/scroll-snap-destination
-  scrollTimeout: 100, // *OPTIONAL* (default = 100) time in ms after which scrolling is considered finished
-  scrollTime: 300 // *OPTIONAL* (default = 300) time in ms for the smooth snap
-}
+  scrollSnapDestination: '100% 0%',
+  scrollTimeout: 10,
+  scrollTime: 500
+};
 
-function callback () {
-  console.log('called when snap animation ends')
+function callback() {
+  console.log('called when snap animation ends');
 }
 
 const element = document.getElementById('container');
-console.log(element);
-const snapObject = new ScrollSnap(element, snapConfig)
-console.log(snapObject);
+const snapObject = new ScrollSnap(element, snapConfig);
+snapObject.bind(callback);
 
-snapObject.bind(callback)
 Kaleidoscope();
