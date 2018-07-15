f8f8cf264269872484ec2cdfaebae24f8777e9ae
HydraFire <hydrafire.plus@gmail.com>
2018-07-15 16:38:21 +0300

    test

--- mobile/components/clientTimeout.js
+++ mobile/components/clientTimeout.js
@@ -5,19 +5,26 @@ function clientTimeout(arr) {
   console.log('lol')
   const audio = document.getElementById('audio');
   let r = 0;
+  audio.src =`${process.env.FILESERVER}22Hz.mp3`;
+  audio.addEventListener('ended',()=>{
+    audio.play();
+  })
+  audio.play();
   const int = setInterval(() => {
 
     if (r === 0) {
-      audio.src =`${process.env.FILESERVER}22Hz.mp3`;
+
     }
 
     if (r >= (parseFloat(arr[1]) * 6)) {
+      audio.removeEventListener('ended',()=>{
+        audio.play();
+      })
       aska(arr[0]);
       clearInterval(int);
     } else {
       r += 1;
-       aska('ой');
     }
-  }, 20000);
+  }, 10000);
 }
 export default clientTimeout;
