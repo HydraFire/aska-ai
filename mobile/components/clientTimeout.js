6447eb198b5cc3e29b29f5a11e83778ffa4932cb
HydraFire <hydrafire.plus@gmail.com>
2018-07-15 16:52:45 +0300

    lol

--- mobile/components/clientTimeout.js
+++ mobile/components/clientTimeout.js
@@ -2,29 +2,25 @@ import { aska } from './speechSynthesizer';
 
 function clientTimeout(arr) {
   arr = JSON.parse(arr);
-  console.log('lol')
+
   const audio = document.getElementById('audio');
-  let r = 0;
-  audio.src =`${process.env.FILESERVER}22Hz.mp3`;
-  audio.addEventListener('ended',()=>{
+
+  audio.src = `${process.env.FILESERVER}20Hz.mp3`;
+  function lol() {
     audio.play();
-  })
+  }
+  audio.addEventListener('ended', lol, false);
   audio.play();
+
+  let r = 0;
   const int = setInterval(() => {
-
-    if (r === 0) {
-
-    }
-
-    if (r >= (parseFloat(arr[1]) * 6)) {
-      audio.removeEventListener('ended',()=>{
-        audio.play();
-      })
+    if (r >= (parseFloat(arr[1]) * 2)) {
+      audio.removeEventListener('ended', lol, false);
       aska(arr[0]);
       clearInterval(int);
     } else {
       r += 1;
     }
-  }, 10000);
+  }, 30000);
 }
 export default clientTimeout;
