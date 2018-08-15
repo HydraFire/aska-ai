b6f082a2c4ef801d530bc31c9886efbc56b3aa98
HydraFire <hydrafire.plus@gmail.com>
2018-08-15 13:09:41 +0300

    fix logo

--- mobile/components/interface/logo.js
+++ mobile/components/interface/logo.js
@@ -8,6 +8,7 @@ function checkOrientation() {
   //  if(window.orientation !== previousOrientation){
   //      previousOrientation = window.orientation;
   //  }
+  window.myconsole.log(`window.orientation = ${window.orientation}`, 'str');
   if (window.orientation === 90 || window.orientation === 270) {
     document.querySelectorAll('.circle').forEach(v => v.className = 'circle-portable');
     document.querySelector('.circle-static').className = 'circle-static-portable';
