6dc06b82f9d86e8a1b70159cc5f2f5af21c7aeee
HydraFire <hydrafire.plus@gmail.com>
2018-08-23 14:20:37 +0300

    fix

--- aska_script/asyncAsk.js
+++ aska_script/asyncAsk.js
@@ -29,8 +29,8 @@ module.exports.whatToSay = whatToSay;
 
 function selectFunctionFromWords(ws, options, defaultFunction) {
   ws.NNListen = false;
-  let skazanoe = 'none';
-  ws.ClientSay = '';
+  ws.ClientSay = 'none';
+  let skazanoe = ws.ClientSay;
   let flag = 0;
   const int = setInterval(() => {
     if (skazanoe !== ws.ClientSay) {
