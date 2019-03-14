e8927ac08858cbf17047828cd22e328e9a60ade8
HydraFire <hydrafire.plus@gmail.com>
2019-03-14 14:31:55 +0200

    event when you take off your headphones

--- mobile/components/asmrControls.js
+++ mobile/components/asmrControls.js
@@ -17,6 +17,7 @@ function randomDuration(audio) {
 }
 
 function playAsmr(state) {
+  window.myconsole.log('not first time, audio3 ended', 'err');
   typeof state != 'string' ? state = 'play' : '';
   const audio = document.getElementById('audio3');
   //console.log(`state = ${state}`);
