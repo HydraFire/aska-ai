62344c2fc26a75287f00ef334ee59a3f3013f549
HydraFire <hydrafire.plus@gmail.com>
2020-01-04 11:24:57 +0200

    test

--- aska_script/commands/WebInteract/checkMoney.js
+++ aska_script/commands/WebInteract/checkMoney.js
@@ -12,21 +12,16 @@ const fileOption = './data/commands/WebInteract/option.json';
 const AskaSC = JSON.parse(fs.readFileSync(fileOption));
 // /////////////////////////////////////////////////////////////////////////////
 function xmltoJson( xml ) {
-
+  console.log(xml);
   if (xml.includes('invalid')) {
     return 'непонятно.0'
   }
-  console.log(xml);
+  console.log(xml); // 159.224.183.122
 
-  xml = xml.substring(xml.search('>') + 1, xml.length);
-  xml = xml.substring(xml.search('>') + 1, xml.length);
-  let arr = [];
-  while (xml.length > 20) {
-    xml = xml.substring(xml.search('>') + 1, xml.length);
-    arr.push( xml.substring(0, xml.search('<')));
-    xml = xml.substring(xml.search('>') + 1, xml.length);
-  }
-  return 'нужно доделать.0';
+  xml = xml.substring(xml.search('balance') + 7, xml.length);
+  xml = xml.substring(0, xml.search('<') + 1);
+
+  return xml;
 }
 
 function sayApiError(ws) {
